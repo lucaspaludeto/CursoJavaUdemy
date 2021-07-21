dc7696e67ecf2d67bc328608594b65a8e6f2d779
Lucas Paludeto <67382997+lucaspaludeto@users.noreply.github.com>
2021-07-21 16:25:21 -0300

    Aula OO

---
+++ exercicios/src/oo/encapsulamento/casaA/Ana.java
@@ -0,0 +1,11 @@
+package oo.encapsulamento.casaA;
+
+public class Ana {
+	
+	//ATRIBUTOS
+	@SuppressWarnings("unused")
+	private String segredo = "...";
+	String facoDentroDeCasa = "..."; //DEFAULT ou PACOTE
+	protected String formaDeFalar = "...";
+	public String todosSabem = "...";
+}
